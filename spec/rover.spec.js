cc88ce65230c5b5e00eb2e01ad76d598b10cd6fd
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-02-26 20:18:16 -0600

    working on test 11

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -12,15 +12,40 @@ describe("Rover class", function() {
     expect(rover.mode).toBe('NORMAL'); 
     expect(rover.generatorWatts).toBe(110); 
   });
-//“response returned by receiveMessage contains the name of the message”
+
   it("response returned by receiveMessage contains the name of the message", function(){
     let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
     let message = new Message('Test 8 name', commands);
     let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-    let response = rover.receivesMessage(message);
+    let response = rover.receiveMessage(message);
     expect(response.message).toBe('Test 8 name');
-  })
-  
+})
+
+it('response returned by receiveMessage includes two results if two commands are sent in the message', function(){
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test 8 name', commands);
+  let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+  let response = rover.receiveMessage(message);
+  expect(response.results.length).toBe(2);
+})
+
+it('responds correctly to the status check command', function(){
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test 8 name', commands);
+  let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+  let response = rover.receiveMessage(message);
+  expect(response.results[1].roverStatus).toEqual({ position: 98382, mode: 'NORMAL', generatorWatts: 110 });
+})
+
+it('responds correctly to the mode change command', function(){
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test 8 name', commands);
+  let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+  let response = rover.receiveMessage(message);
+  let modeChangeResult = response.results.find(result => result.completed && result.roverStatus);
+
+  expect(modeChangeResult.roverStatus.mode).toBe('LOW_POWER');
+});
 
 });
 
